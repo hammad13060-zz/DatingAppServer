199be21464afbd31a77d31b6b71c7c0dc8e50fd3
hammad13060 <hammad13060@iiitd.ac.in>
2015-11-18 16:21:11 +0530

    sending like data to tha app for caching purpose

--- register_user.php
+++ register_user.php
@@ -1,11 +1,32 @@
 <?php 
 
+	$servername = "127.0.0.1";
+	$username = "root";
+	$password = "";
+	$dbname = "dating_application";
+
+
+	//connecting with database
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+
 	//reading the request body
 	$entityBody = file_get_contents('php://input');
 
 	//decoding the request body and returning json as php array using true as second argument
 	$json_request = json_decode($entityBody, true);
 	$json_response = array();
+
+
+
+
+
 
 	$user_id = $json_request["user_id"]; //string type
 	$name = $json_request["name"]; //string type
@@ -15,39 +36,44 @@
 
 	$registered = False;
 
-	$dbconection = mysql_connect("127.0.0.1", "root", "");
-	mysql_select_db("dating_application",$dbconection);
+	$query = "SELECT * FROM users WHERE user_id = '$user_id';";
 
-	$query = mysql_query("SELECT * FROM users WHERE user_id = '$user_id'", $dbconection);
+	$result = $conn->query($query);
 
-	$row = mysql_fetch_array($query);
-
-	if($row !== False)
+	if($result->num_rows > 0)
 	{
 		//update
 		$registered = True;
-		$queryNew = mysql_query("UPDATE users SET name = '$name', gender = '$gender', age = '$age', url = '$url' WHERE user_id = '$user_id'");
+		$update_query = "UPDATE users SET name = '$name', gender = '$gender', age = '$age', url = '$url' WHERE user_id = '$user_id';";
+		$conn->query($update_query);
+
+		$like_query = "SELECT user_id_2 FROM likes WHERE user_id_1='$user_id';";
+		$like_query_result = $conn->query($like_query);
+		if ($like_query_result->num_rows > 0) {
+			$like_array = array();
+			$i = 0;
+			while ($row = $like_query_result->fetch_assoc()) {
+				$like_array[$i++] = $row['user_id_2'];			
+			}
+			$json_response["likes"] = $like_array;
+		}
 	}
 	else if ($row === False)
 	{
 		//insert
 		$registered = False;
-		$queryNew = mysql_query("INSERT INTO users (user_id,name,gender,age,url) VALUES ('$user_id','$name','$gender','$age','$url')");
-	}
-	else
-	{
-		echo mysql_error();
+		$queryNew = "INSERT INTO users (user_id,name,gender,age,url) VALUES ('$user_id','$name','$gender','$age','$url');";
+		$conn->query($queryNew);
 	}
 
 	$json_response["registered"] = $registered;
 
 
-	mysql_close($dbconection);
+	$conn->close();
 
 	//encoding json to response body
 	$json_response_encoded = json_encode($json_response);
 	//setting the content type tp json object
 	header('Content-type: application/json');
 	exit($json_response_encoded);
-
 ?>
